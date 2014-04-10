05c0f06690b743aa2f08d9902824db2124e15c7f
U-Sonata\Keg <Keg@Sonata.(none)>
2014-04-09 18:30:09 -0700

    cleaned simple affine test

--- testAffineSimple.py
+++ testAffineSimple.py
@@ -3,50 +3,50 @@ import numpy as np
 
 n = 3
 
-print('scalar var')
+print('\nscalar var')
 z = Variable()
 z.printme()
 
-print('vector var')
+print('\nvector var')
 y = Variable(n)
 y.printme()
 
-print('matrix var')
+print('\nmatrix var')
 x = Variable(n,n)
 x.printme()
 
-print('scalar*variable')
+print('\nscalar*variable')
 (4*z).printme()
 (4*y).printme()
 (4*x).printme()
 
-print('matrix*variable')
+print('\nmatrix*variable')
 A = np.ones(n)
 (A*y).printme()
 (A*x).printme()
 
-print('basic ops')
+print('\nbasic ops')
 y2 = Variable(n)
 x2 = Variable(n,n)
 
-(y+y2).printme()
-(x+x2).printme()
+(y-y2).printme()
+(x-x2).printme()
 (A*(x+x2)).printme()
 
-print('with constant')
+print('\nwith constant')
 (y+5).printme()
-b = np.array([[1],[2],[7]])
+b = np.array([[1,2,7]]).T
 (y+b).printme()
 
 c = np.array(range(0,9)).reshape((3,3))
 (x+c).printme()
 
-print('slicing')
+print('\nslicing')
 y[0:3:2].printme()
 x[0:3:2,1:3].printme()
 q = Variable(1,n)
 (q[0:3:2] + q[1:]).printme()
 
 if False:
-	print('test large')
+	print('\ntest large')
 	w = Variable(10000000)
