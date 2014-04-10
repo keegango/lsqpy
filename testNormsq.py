2023c073848c675e3cb37e5657c174ce56aa2d3b
U-Sonata\Keg <Keg@Sonata.(none)>
2014-04-09 18:34:07 -0700

    renamed normsq test example and fixed code

--- testNormsq.py
+++ testNormsq.py
@@ -5,21 +5,15 @@ Test for norm square expressions
 from lsqpy.exprs.variable import Variable
 from lsqpy.exprs.normsq import normsq
 
-def printAffine(aff):
-	for key in aff.coefs:
-		print("AFFINE_CONST") if key == 1 else print(key)
-		print(aff.coefs[key].todense())
-	print('------------------------------')
-
 def printns(ns):
 	print('has '+str(len(ns.sq_terms))+' sq terms')
 	for aff in ns.sq_terms:
-		printAffine(aff)
-		print('****************************')
+		aff.printme()
+		print('******************************************')
 
 z = Variable(2)
 x = Variable(2)
 
 nse = normsq(z)+2*normsq(x)
 printns(nse)
-print(nse.num_constraints())
+print(nse.num_constraints()) # includes implicit constraints
