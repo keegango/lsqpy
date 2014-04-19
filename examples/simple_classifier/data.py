4ac28c4bfd3265fbd412b8272910b4fc24fd6674
U-Sonatina\Keegan <Keegan@Sonatina.(none)>
2014-04-19 14:05:40 -0700

    changed data file to plot if called

--- examples/simple_classifier/data.py
+++ examples/simple_classifier/data.py
@@ -1,16 +1,21 @@
 import numpy as np
 
+# Set the random seed to get consistent data
 np.random.seed(0)
 
+# Number of examples to use
 n = 200
 
-true_vect = np.array([[-0.7071,0.7071]]).T
+# Specify the true value of the variable
+true_vect = np.array([[-1,1]]).T
 
+# Create data and labels
 X = np.random.standard_normal(size=(n,2))*2
 y = np.sign(X.dot(true_vect)+np.random.standard_normal(size=(n,1)))
 
-# Set to True if you want to plot the data
-if(False):
+# Plot data if python was called on this file directly
+import sys
+if(sys.argv[0] == 'data.py'):
 	import matplotlib.pyplot as plt
 	plt.plot(X[y[:,0]>=0,0],X[y[:,0]>=0,1],'ro')
 	plt.plot(X[y[:,0]<0,0],X[y[:,0]<0,1],'bo')
